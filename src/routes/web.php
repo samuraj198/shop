ce8e7f4afc488538b39a30dcebc6096ee4c71083
samuraj198 (Daniil Rakhimzyanov) <daniil885@mail.ru>
2025-12-04 14:58:25 +0400

    refactor routes in web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -8,19 +8,28 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\MainPageController;
 
 Route::get('/', [MainPageController::class, 'index']);
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::delete('/logout', [AuthController::class, 'logout']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::put('/categories/{category}', [CategoryController::class, 'update']);
-Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::put('/products/{product}', [ProductController::class, 'update']);
-Route::delete('/products/{product}', [ProductController::class, 'destroy']);
 
-Route::get('/cart', [ProductController::class, 'index']);
-Route::post('/cart/{product}', [CartController::class, 'store']);
-Route::put('/cart/{cartItem}', [CartController::class, 'update']);
-Route::delete('/cart/{cartItem}', [CartController::class, 'destroy']);
+Route::prefix('/categories')->group(function () {
+    Route::post('/', [CategoryController::class, 'store']);
+    Route::put('/{category}', [CategoryController::class, 'update']);
+    Route::delete('/{category}', [CategoryController::class, 'destroy']);
+});
+
+Route::prefix('/products')->group(function () {
+    Route::post('/', [ProductController::class, 'store']);
+    Route::put('/{product}', [ProductController::class, 'update']);
+    Route::delete('/{product}', [ProductController::class, 'destroy']);
+});
+
+Route::prefix('/cart')->group(function () {
+    Route::get('/', [CartController::class, 'index']);
+    Route::post('/{product}', [CartController::class, 'store']);
+    Route::put('/{cartItem}', [CartController::class, 'update']);
+    Route::delete('/{cartItem}', [CartController::class, 'destroy']);
+});
 
 Route::get('/catalog', [CatalogController::class, 'index']);
